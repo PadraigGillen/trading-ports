3b7d152889c6820e97e02ce0eae874a47028d602
Erik <watterson.erik@gmail.com>
2019-07-02 01:25:31 +0000

    Updating comments with route information.

--- server/tp_server.py
+++ server/tp_server.py
@@ -1,7 +1,95 @@
+"""
+To Run:
+    
+    export FLASK_RUN_PORT=####
+    export FLAS_APP=tp_server.py
+    flask run
+
+"""
 #!/usr/bin/env python
 from flask import Flask
+from flask_restful import Resource, Api
 app = Flask(__name__)
+
+
+
+"""
+[GET REQUESTS]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@get info about boat
+.../boat/{ID}
+
+@get info about all boats
+...
+
+@get info about user
+
+@get info about all users
+
+@get info about ocean
+- DEPRICATED
+
+@get info about everything
+
+@get info about boat
+
+@get info about all boats
+
+[BOAT MANIPULATION]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@create boat
+.../boat (post)
+ - Boat attributes are determined via the server
+    - likely random
+
+@move a boat
+.../boat/{ID}/{location}/{ID} (put)
+- Location can be:
+    sea (ID: 0)
+    harbor (ID)
+    slip (ID)
+- Location must be considered connected to current location:
+    slip <-> harbor <-> sea
+- timed event based on daemon
+
+@delete a boat
+.../boat/{ID} (delete)
+- determined by daemon
+    - calculated based on total number of boats
+
+@update a boat
+.../boat/{ID} (Patch)
+
+@delete all boats
+.../boat (delete)
+- will require auth
+
+[Port manipulation]
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+@delete a slip
+.../slip/{ID} (delete)
+- requires auth
+- cannot be used if there is a boat in the slip
+
+@create a slip
+.../slip (post)
+- requires auth
+
+
+"""
 
 @app.route("/")
 def intro():
     return "<h1>Welcome to our boat site</h1>"
+
+
+
+
+
+
+
+
+api.add_resource(main, '/')
+
+
+
